22e3777b93fd4fd63a6f5119dd29f47c0eaa3d33
Kemal1603 <prince_of_love93@mail.ru>
2021-01-08 21:25:43 +0300

    Quizz

---
+++ main.py
@@ -0,0 +1,17 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank = []
+for index in range(len(question_data)):
+	new_question = Question(question_data[index]['text'], question_data[index]['answer'])
+	question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+
+
+while quiz.still_has_questions():
+	quiz.next_question()
+
+print("You've completed the quiz")
+print(f"Your final score was {quiz.score}/{quiz.question_number}")
